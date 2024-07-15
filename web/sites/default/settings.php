17754aa388fd95d081c4090b8e16ede41d36266c
Mark Fullmer <jfullmer@austin.utexas.edu>
2024-07-15 10:52:49 -0500

    162 - Ability to block IP addresses (#169)
    * Add ability to block IP addresses
    
    * Avoid deprecated list() syntax
    
    * Fix typo
    
    * Ensure a boolean return value is present regardless of presence of file
    
    * Add check for file

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -47,6 +47,60 @@
  * Developers should not directly modify this file, but rather should use context-specific
  * PHP includes to override or supplement these defaults (see the list of pre-defined
  * includes, below).
+*/
+
+// BEGIN IP ADDRESS BLOCKING.
+// Check the site's private filesystem for the presence of an IP block file.
+$blocked_ip_filepath = __DIR__ . '/files/private/blocked_ips.txt';
+/**
+ * Check if a specified IP address is in a blocklist.
+ *
+ * @param string $request_remote_addr
+ *   An IPv4 value.
+ * @param string $blocked_ip_filepath
+ *   Path to a valid file location.
+ *
+ * @return bool
+ *   Whether or not the IP address is blocked.
+ */
+function ip_is_blocked($request_remote_addr, $blocked_ip_filepath) {
+  $request_ip_forbidden = FALSE;
+  if (!file_exists($blocked_ip_filepath)) {
+    return FALSE;
+  }
+  if ($file_contents = file($blocked_ip_filepath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES)) {
+    // Blocked addresses are entered one per line, formatted as
+    // IPv4: Single IPs and CIDR.
+    // See https://en.wikipedia.org/wiki/Classless_Inter-Domain_Routing
+    // Convert the list into a PHP array.
+    $request_ip_blocklist = $file_contents;
+    // Check if this IP is an exact match in the blocklist.
+    if (!$request_ip_forbidden = in_array($request_remote_addr, $request_ip_blocklist)) {
+      // Check if this IP is in CIDR block list.
+      foreach ($request_ip_blocklist as $_cidr) {
+        // A CIDR is identified by having the presence of a /.
+        // Only evaluate lines that have a / and do not have # or characters.
+        if (strpos($_cidr, '/') !== FALSE && strpos($_cidr, '#') === FALSE && strpos($_cidr, ':') === FALSE) {
+          $_ip = ip2long($request_remote_addr);
+          [$_net, $_mask] = explode('/', $_cidr, 2);
+          $_ip_net = ip2long($_net);
+          $_ip_mask = ~((1 << (32 - $_mask)) - 1);
+          if ($request_ip_forbidden = ($_ip & $_ip_mask) == ($_ip_net & $_ip_mask)) {
+            break;
+          }
+        }
+      }
+    }
+  }
+  return $request_ip_forbidden;
+}
+
+
+if (ip_is_blocked($_SERVER['REMOTE_ADDR'], $blocked_ip_filepath)) {
+  header('HTTP/1.0 403 Forbidden');
+  exit;
+}
+// END IP ADDRESS BLOCKING.
 
 /**
  * Load services definition file.
